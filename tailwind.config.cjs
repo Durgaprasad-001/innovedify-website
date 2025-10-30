4b4f03f5064f779b7e56325bfc51d0f01167d497
Durgaprasad-001 <167807011+Durgaprasad-001@users.noreply.github.com>
2025-10-30 23:29:27 +0530

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'inn-blue': '#0d1b2a',
+        'inn-cyan': '#00aaff',
+        'inn-accent': '#07b0ff'
+      },
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+};
